b0096667dcdeb0e15e7113af1342e65918c93573
jpca999 <jpca999@gmail.com>
2017-05-03 15:01:28 -0400

    Hello coming on the screen

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <Text>
+        Hello!
+      </Text>
+    </Provider>
+  );
+};
+
+export default App;
