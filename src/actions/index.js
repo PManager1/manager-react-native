49ef9290999b461a7c15630e030df5056dfbc84e
jpca999 <jpca999@gmail.com>
2017-05-08 16:30:03 -0400

    119 started - I didnt check if its witout errors for 118

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 // when email changes
 export const emailChanged = (text) => {
@@ -21,7 +21,7 @@ export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(user => {
-      dispatch ({ type: 'LOGIN_USER_SUCCESS', payload: user }); 
+      dispatch ({ type: LOGIN_USER_SUCCESS, payload: user });
     });
   };
 };
