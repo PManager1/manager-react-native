7d34a0c086a4a986821681d60981fe0fe317507b
jpca999 <jpca999@gmail.com>
2017-05-09 21:45:53 -0400

    131  employee create form done

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
+
 
 class EmployeeCreate extends Component {
   render(){
     return(
-        <View>
-          <Text>
-            Employee Form
-          </Text>
-        </View>
+      <Card>
+
+      <CardSection>
+        <Input
+          label="Name"
+          placeholder="Jane"
+        />
+      </CardSection>
+
+      <CardSection>
+      <Input
+        label="Phone no"
+        placeholder="999-999-9999"
+      />
+      </CardSection>
+
+      <CardSection>
+        <Button>
+          Create
+        </Button>
+      </CardSection>
+
+      </Card>
     );
   }
 }
-export default EmployeeCreate; 
+export default EmployeeCreate;
