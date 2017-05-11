d97ebb697e5667e4911c07a1487e13ba7d13120e
jpca999 <jpca999@gmail.com>
2017-05-10 20:40:43 -0400

    136 done

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import { Picker } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
@@ -29,7 +29,10 @@ class EmployeeCreate extends Component {
       />
       </CardSection>
 
-      <CardSection>
+      <CardSection style={{ flexDirection: 'column' }} >
+        <Text style={styles.pickerTextStyle}>
+          Shift
+        </Text>
         <Picker
           style={{ flex: 1 }}
           selectedValue={this.props.shift}
@@ -55,6 +58,13 @@ class EmployeeCreate extends Component {
     );
   }
 }
+
+const styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+    }
+};
 
 const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
