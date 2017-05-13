92962c68ef6b40a0683cbe2c7c07f76ae07399d4
jpca999 <jpca999@gmail.com>
2017-05-13 13:39:27 -0400

    153 done

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,14 +2,13 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
   componentWillMount(){
-    //  we understnad that there's an employee model coming into this component,
-    // & we're going to iterate over every property of that object, and update our reducer with every property.
-    //
+//  we understnad that there's an employee model coming into this component,
+// & we're going to iterate over every property of that object, and update our reducer with every property.
     _.each(this.props.employee, (value, prop) => {
         this.props.employeeUpdate ({ prop, value });
     });
@@ -18,7 +17,11 @@ class EmployeeEdit extends Component {
   onButtonPress(){
     const { name, phone, shift } = this.props;
     console.log(name, phone, shift);
+    // action creator to save our new data to firebase.
+          // we'll pass in the modified values of name, phone and etc
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid })
   }
+
   render(){
     return(
         <Card>
@@ -38,4 +41,30 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate }) (EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave }) (EmployeeEdit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
