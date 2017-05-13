1841aeb7b0d3bf886c7af250886ae265bc768e40
jpca999 <jpca999@gmail.com>
2017-05-13 13:08:54 -0400

    152 showing the updated/changed values in the console log

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -9,17 +9,22 @@ class EmployeeEdit extends Component {
   componentWillMount(){
     //  we understnad that there's an employee model coming into this component,
     // & we're going to iterate over every property of that object, and update our reducer with every property.
-    // 
+    //
     _.each(this.props.employee, (value, prop) => {
         this.props.employeeUpdate ({ prop, value });
     });
+  }
+
+  onButtonPress(){
+    const { name, phone, shift } = this.props;
+    console.log(name, phone, shift);
   }
   render(){
     return(
         <Card>
           <EmployeeForm />
             <CardSection>
-              <Button>
+              <Button onPress={this.onButtonPress.bind(this)}>
                 Save changes
               </Button>
             </CardSection>
@@ -28,4 +33,9 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect(null, { employeeUpdate }) (EmployeeEdit);
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate }) (EmployeeEdit);
