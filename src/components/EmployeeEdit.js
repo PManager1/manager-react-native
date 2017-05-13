7b61ac2e6d9df58e561b1fbb274809fe2871221b
jpca999 <jpca999@gmail.com>
2017-05-13 13:02:55 -0400

    152 mid done - cannot save it yet

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,10 +1,19 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
+import { employeeUpdate } from '../actions';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+  componentWillMount(){
+    //  we understnad that there's an employee model coming into this component,
+    // & we're going to iterate over every property of that object, and update our reducer with every property.
+    // 
+    _.each(this.props.employee, (value, prop) => {
+        this.props.employeeUpdate ({ prop, value });
+    });
+  }
   render(){
     return(
         <Card>
@@ -19,4 +28,4 @@ class EmployeeEdit extends Component {
   }
 }
 
-export default connect(null) (EmployeeEdit);
+export default connect(null, { employeeUpdate }) (EmployeeEdit);
